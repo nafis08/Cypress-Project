461e9937e5063fc6d9d9a5ccc239be105e984ed3
Nazmus Saqib Nafis <nafis084438@gmail.com>
2022-01-27 06:53:15 +0600

    Describe added

--- ProjectForAklamio/cypress/integration/first_test.js
+++ ProjectForAklamio/cypress/integration/first_test.js
@@ -7,16 +7,65 @@ import {SignUp, ToSignUp} from "../Elements/Sign_Up"
 //const cypress = require("cypress")
 
 //const website 
-const AssertCheckWebPage = '#main > div > nav > div > a'
-it( 'This Test is for Loading Website', function() {
+
+describe('My Cypress Test Project', function(){
+    it( 'This Test is for Loading Website', function(){
+        const AssertCheckWebPage = '#main > div > nav > div > a'
+        cy.visit('/').wait(7000)
+        cy.get(AssertCheckWebPage).should('have.text', 'conduit') //visit site    
+    })
+
+    it('This Test is for Signing Up by providing any Username, email and Password', function(){
+        ToSignUp.ClickOnSignUpButton()
+        ToSignUp.TypeNameforSignUp()
+        ToSignUp.TypeMailAddressForSignUp()
+        ToSignUp.TypePassWordForSignUp()
+        ToSignUp.WaitForDashBoardToLoad()
+        ToSignUp.AssertionCheck()
+    })
+
+    it('This Test is for creating New Post', function() {
+        toNewPost.ClickOnNewPostButton()
+        toNewPost.AssertionCheckBeforeNewPost()
+        toNewPost.TypeNewPostTitle()
+        toNewPost.TypePostDescription()
+        toNewPost.TypePostBody()
+        toNewPost.TypeTagsOfPost()
+        toNewPost.PostArticle()
+        toNewPost.WaitForArticleToPost()
+        toNewPost.AssertionCheckAfterNewPost()
+    })
+
+    it('This test is to Post Comment', function() {
+        toComment.WriteComment()
+        toComment.PostComment()
+        toComment.WaitForComment()
+        toComment.ViewComment()
+    })
+
+    it('This test is for Deleting Post', function() {
+        toDeletePost.DeletePost()
+        toDeletePost.WaitForDashBoard()
+        ToSignUp.AssertionCheck()
+    })
+
+    it('This test is for Logging Out', function(){
+        toLogOut.ClickOnSettingsButton()
+        toLogOut.ScrollToLogOutButton()
+        toLogOut.ClickOnLogOut()
+        toLogOut.AssertionCheckOfLogOut()
+    })
+})
+
+/*it( 'This Test is for Loading Website', function() {
     cy.visit('/').wait(7000)
     cy.get(AssertCheckWebPage).should('have.text', 'conduit') //visit site    
     //assert.equal(cy.get('.navbar-brand'))
-})
+})*/
 
 //const Sign_Up = new SignUp()
 
-it('This Test is for Signing Up by providing any Username, email and Password', function(){
+/*it('This Test is for Signing Up by providing any Username, email and Password', function(){
     ToSignUp.ClickOnSignUpButton()
     ToSignUp.TypeNameforSignUp()
     ToSignUp.TypeMailAddressForSignUp()
@@ -57,7 +106,7 @@ it('This test is for Logging Out', function(){
     toLogOut.AssertionCheckOfLogOut()
 })
 
-
+*/
 
 
 
